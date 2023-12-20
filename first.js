e333673988b661174a82acda40ec3f719cb4f843
Judith Hurzlmeier <judith.hurzlmeier@deutschebahn.com>
2023-12-20 16:26:11 +0100

    Add first file
    This file is for basci code structure exercises.

---
+++ first.js
@@ -0,0 +1,23 @@
+function sayHello(name) {
+    console.log(`Hi dear ${name}`)
+}
+
+sayHello('Weihnachtsmann')
+// say hello to a friend
+
+
+// initialize the best name ever =  give it the initial value and greet them
+// const bestNameEver = ;
+
+
+
+let bestCollegueEver;
+// who is the best colleague? greet them
+
+
+
+// but maybe your favorite changes?
+// bestCollegueEver = ;
+
+// let's greet all of our colleagues
+
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 export default function grow(x){
-    console.log(x)
+    console.log(typeof x)
   }
 
 
   grow(27)
+  grow("Test")
+  grow([6,78,3])
