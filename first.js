ba9f126a02b002c30e618ae0aa4130152d004fcd
Judith Hurzlmeier <judith.hurzlmeier@deutschebahn.com>
2023-12-20 17:25:39 +0100

    Extend js basics
    - array operations
    - if
    - for

--- first.js
+++ first.js
@@ -10,7 +10,6 @@ sayHello('Weihnachtsmann')
 // const bestNameEver = ;
 
 
-
 let bestCollegueEver;
 // who is the best colleague? greet them
 
@@ -20,4 +19,19 @@ let bestCollegueEver;
 // bestCollegueEver = ;
 
 // let's greet all of our colleagues
+const allColleagues = ["Patrik", "Suresh", "Robin", "Daniel", "Corinna", "Tito", "Nico"]
+sayHello(allColleagues)
 
+// only greet the third colleague
+
+// greet colleagues in alphabetical order
+sayHello(allColleagues.sort())
+
+// greet only colleagues which have an 'i' in their name
+for (let name of allColleagues) {
+    if (name.includes('i')) {
+        sayHello(name)
+    }
+}
+
+// greet names which have more than 5 characters
